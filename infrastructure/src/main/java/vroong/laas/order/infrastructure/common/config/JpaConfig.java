6cc2366adeedd65399556e47debc78e8b48178b4
YoungChulShin-Russel <youngchul.shin@meshkorea.net>
2025-10-02 08:15:19 +0900

    jpa 오류 수정

--- infrastructure/src/main/java/vroong/laas/order/infrastructure/common/config/JpaConfig.java
+++ infrastructure/src/main/java/vroong/laas/order/infrastructure/common/config/JpaConfig.java
@@ -1,7 +1,5 @@
 package vroong.laas.order.infrastructure.common.config;
 
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -9,7 +7,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @Configuration
 @EntityScan(basePackages = "vroong.laas.order.infrastructure.storage.db")
 @EnableJpaRepositories(basePackages = "vroong.laas.order.infrastructure.storage.db")
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
-public class JpaConfig {
+class JpaConfig {
 
 }
